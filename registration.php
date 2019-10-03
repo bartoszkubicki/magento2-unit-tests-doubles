6c9d0e239cdaad9b950fc4d327fb921a91c2578c
Bartosz Kubicki <bartosz.kubicki@lizardmedia.pl>
2019-10-03 16:38:22 +0200

    Updating version

--- registration.php
+++ registration.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
+    ComponentRegistrar::LIBRARY,
     'BKubicki_Magento2TestDoubles',
     __DIR__ . '/src'
 );
